ae98b236978741e7796afc295da3a0b070799b24
UsedToTalkALOt <ayushadhikari64209@gmail.com>
2023-06-12 08:25:24 +0545

    checking

---
+++ src/model/MyConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+
+
+import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+    public static Connection dbConnect()
+    {
+        try
+        {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/trackhub","root","cpktnwt");
+            System.out.println("connected to database");
+        return conn;
+        }
+        catch(Exception e)
+        {
+        }
+        return null;
+    }
+
+
+}
